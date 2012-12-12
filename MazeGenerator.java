e26e2f12503de46185f3b7357aa6b4c66a42c932
tkg2104 <tkg2104@columbia.edu>
2012-12-12 16:05:53 -0500

    Update MazeGenerator.java

--- MazeGenerator.java
+++ MazeGenerator.java
@@ -11,7 +11,7 @@ public class MazeGenerator
 	private static final Random rand = new Random();
  
 	public MazeGenerator(int x, int y) 
-  {
+  	{
 		this.x = x;
 		this.y = y;
 		maze = new int[this.x][this.y];
@@ -19,41 +19,40 @@ public class MazeGenerator
 	}
  
 	public void display() 
-  {
+  	{
 		for (int i = 0; i < y; i++) 
-    {
+    		{
 			// draw the north edge
 			for (int j = 0; j < x; j++) 
-      {
+      			{
 				System.out.print((maze[j][i] & 1) == 0 ? "+---" : "+   ");
 			}
 			System.out.println("+");
 			// draw the west edge
 			for (int j = 0; j < x; j++) 
-      {
+      			{
 				System.out.print((maze[j][i] & 8) == 0 ? "|   " : "    ");
 			}
 			System.out.println("|");
 		}
 		// draw the bottom line
 		for (int j = 0; j < x; j++) 
-    {
+    		{
 			System.out.print("+---");
 		}
 		System.out.println("+");
 	}
  
 	private void generateMaze(int cx, int cy) 
-  {
+  	{
 		DIR[] dirs = DIR.values();
 		Collections.shuffle(Arrays.asList(dirs));
 		for (DIR dir : dirs) 
-    {
+    		{
 			int nx = cx + dir.dx;
 			int ny = cy + dir.dy;
-			if (between(nx, x) && between(ny, y)
-					&& (maze[nx][ny] == 0)) 
-      {
+			if (between(nx, x) && between(ny, y) && (maze[nx][ny] == 0)) 
+      			{
 				maze[cx][cy] |= dir.bit;
 				maze[nx][ny] |= dir.opposite.bit;
 				generateMaze(nx, ny);
@@ -62,12 +61,12 @@ public class MazeGenerator
 	}
  
 	private static boolean between(int v, int upper) 
-  {
+  	{
 		return (v >= 0) && (v < upper);
 	}
  
 	private enum DIR 
-  {
+  	{	
 		N(1, 0, -1), S(2, 0, 1), E(4, 1, 0), W(8, -1, 0);
 		private final int bit;
 		private final int dx;
@@ -76,7 +75,7 @@ public class MazeGenerator
  
 		// use the static initializer to resolve forward references
 		static 
-    {
+    		{
 			N.opposite = S;
 			S.opposite = N;
 			E.opposite = W;
@@ -84,7 +83,7 @@ public class MazeGenerator
 		}
  
 		private DIR(int bit, int dx, int dy) 
-    {
+    		{
 			this.bit = bit;
 			this.dx = dx;
 			this.dy = dy;
@@ -92,7 +91,7 @@ public class MazeGenerator
 	};
  
 	public static void main(String[] args) 
-  {
+  	{
 		int x = args.length >= 1 ? (Integer.parseInt(args[0])) : 8;
 		int y = args.length == 2 ? (Integer.parseInt(args[1])) : 8;
 		MazeGenerator maze = new MazeGenerator(x, y);
