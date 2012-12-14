ecc7de5c216b3d23f6a6acf34fa89bf9474295f4
nhelferty <nikhil.helferty@gmail.com>
2012-12-14 01:33:25 -0500

    Update AMLJava.java

--- AMLJava.java
+++ AMLJava.java
@@ -31,29 +31,100 @@ public class AMLJava extends JFrame
     static JTextArea textArea;
     
     // build the representation of the maze from the text file
-    public static void buildMaze() {
-        File mazeFile  = new File("maze.txt");
-        try {
-            Scanner scan = new Scanner(mazeFile);
-            width = scan.nextInt();
-            height = scan.nextInt();
-            maze = new Cell[height][width];
-            for (int row = 0; row < height; row++) {
-                for (int col = 0; col < width; col++) {
-                    int temp = scan.nextInt();
-                    maze[row][col] = new Cell(temp, row, col);
-                    if (temp == 2) current = maze[row][col];
+    public static void buildMaze(String mazeFileName) {
+        if (mazeFileName.equals("random")) randomGenMaze();
+        else {
+            File mazeFile  = new File(mazeFileName);
+            try {
+                Scanner scan = new Scanner(mazeFile);
+                width = scan.nextInt();
+                height = scan.nextInt();
+                maze = new Cell[height][width];
+                for (int row = 0; row < height; row++) {
+                    for (int col = 0; col < width; col++) {
+                        int temp = scan.nextInt();
+                        maze[row][col] = new Cell(temp, row, col);
+                        if (temp == 2) current = maze[row][col];
+                    }
                 }
+                scan.close();
             }
-            scan.close();
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("File Not Found");
-            return;
+            catch (FileNotFoundException e) {
+                System.out.println("File Not Found");
+                return;
+            }
         }
         moves = new Stack<Cell>();
         new AMLJava(); // initiliase the Swing GUI
     }
+    
+    public static void randomGenMaze() {
+        width = (int)(Math.random() * 20) + 5;
+        height = (int)(Math.random() * 20) + 5;
+        maze = new Cell[height][width];
+        int targetRow = (int)(Math.random()*(height-1));
+        int targetCol = (int)(Math.random()*(width-1));
+        maze[targetRow][targetCol] = new Cell(3, targetRow, targetCol); // randomly generate a target cell
+        int stepLength = (int)((Math.random()*width)/2 + (Math.random()*height)/2) + width/2 + height/2; // determine how many steps it'll iterate back from the target until a start cell is picked
+        boolean pathGenerated = false;
+        while (!pathGenerated) pathGenerated =  pathGen(maze[targetRow][targetCol], stepLength);
+        // start point has been picked - for remaining null cells, randomly pick between a step or a hole
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (maze[row][col] == null) {
+                    double prob = Math.random();
+                    if (prob < .3) maze[row][col] = new Cell(1, row, col);
+                    else maze[row][col] = new Cell(0, row, col);
+                }
+            }
+        }
+    }
+    
+    public static boolean pathGen(Cell c, int steps) {
+        int cRow = c.getRow();
+        int cCol = c.getCol();
+        if (steps == 0) { // make this cell the starting one
+            maze[cRow][cCol] = new Cell(2, cRow, cCol);
+            return true;
+        }
+        LinkedList<String> dirs = new LinkedList<String>();
+        dirs.add("up");
+        dirs.add("down");
+        dirs.add("left");
+        dirs.add("right");
+        if (cRow == 0) dirs.remove("up");
+        else if (maze[cRow-1][cCol] !=  null) dirs.remove("up");
+        if (cRow == (height-1)) dirs.remove("down");
+        else if (maze[cRow+1][cCol] != null) dirs.remove("down");
+        if (cCol == 0) dirs.remove("left");
+        else if (maze[cRow][cCol-1] != null) dirs.remove("left");
+        if (cCol == (width-1)) dirs.remove("right");
+        else if (maze[cRow][cCol+1] != null) dirs.remove("right");
+        if (dirs.size() == 0) return false;
+        String randDir = dirs.get((int)(Math.random() * dirs.size())); // pick a random direction from the current cell
+        if (randDir.equals("up")) {
+            maze[cRow-1][cCol] = new Cell(1, cRow, cCol);
+            steps--;
+            return pathGen(maze[cRow-1][cCol], steps);
+        }
+        else if (randDir.equals("down")) {
+            maze[cRow+1][cCol] = new Cell(1, cRow+1, cCol);
+            steps--;
+            return pathGen(maze[cRow+1][cCol], steps);
+        }
+        else if (randDir.equals("left")) {
+            maze[cRow][cCol-1] = new Cell(1, cRow, cCol-1);
+            steps--;
+            return pathGen(maze[cRow][cCol-1], steps);
+        }
+        else {
+            maze[cRow][cCol+1] = new Cell(1, cRow, cCol+1);
+            steps--;
+            return pathGen(maze[cRow][cCol+1], steps);
+        }
+        
+    }
+    
     
     // creates the Swing GUI
     public AMLJava() {
