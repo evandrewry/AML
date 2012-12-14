9618f405471b592c6a7a22274bddb3bbc643ead2
nhelferty <nikhil.helferty@gmail.com>
2012-12-14 01:34:22 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,12 +2,17 @@
 public class Test
 {
     public static void main(String [] args) throws Exception {
-        AMLJava.buildMaze();
+        AMLJava.buildMaze("maze.txt");
         AMLJava.move_U();
         AMLJava.move_L();
         AMLJava.move_D();
         AMLJava.move_R();
         AMLJava.revert();
         AMLJava.revert();
-    }        
+    } 
+    
+    public static void testList() {
+        List x = new List(new Object[] {0, 1, 5, 7});
+        List list = new List(new Object [] {new List(new Object[] {0, 1, 5, 7}), new List(new Object[] {0, 1, 5, 7})});
+    }
 }
