97273af081cb9bcfd6f4cd5a64e5a34e9f21af9a
nhelferty <nikhil.helferty@gmail.com>
2012-12-17 16:51:35 -0500

    Update AMLJava.java

--- AMLJava.java
+++ AMLJava.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.text.*;
 
 /*
  * Standard Library of Java code for amL
@@ -84,7 +85,7 @@ public class AMLJava extends JFrame
         int cRow = c.getRow();
         int cCol = c.getCol();
         if (steps == 0) { // make this cell the starting one
-            maze[cRow][cCol] = new Cell(2, cRow, cCol);
+            current = maze[cRow][cCol] = new Cell(2, cRow, cCol);
             return true;
         }
         LinkedList<String> dirs = new LinkedList<String>();
@@ -93,32 +94,28 @@ public class AMLJava extends JFrame
         dirs.add("left");
         dirs.add("right");
         if (cRow == 0) dirs.remove("up");
-        else if (maze[cRow-1][cCol] !=  null) dirs.remove("up");
         if (cRow == (height-1)) dirs.remove("down");
-        else if (maze[cRow+1][cCol] != null) dirs.remove("down");
         if (cCol == 0) dirs.remove("left");
-        else if (maze[cRow][cCol-1] != null) dirs.remove("left");
         if (cCol == (width-1)) dirs.remove("right");
-        else if (maze[cRow][cCol+1] != null) dirs.remove("right");
         if (dirs.size() == 0) return false;
         String randDir = dirs.get((int)(Math.random() * dirs.size())); // pick a random direction from the current cell
         if (randDir.equals("up")) {
-            maze[cRow-1][cCol] = new Cell(1, cRow, cCol);
+            if (maze[cRow-1][cCol] == null) maze[cRow-1][cCol] = new Cell(1, cRow-1, cCol);            
             steps--;
             return pathGen(maze[cRow-1][cCol], steps);
         }
         else if (randDir.equals("down")) {
-            maze[cRow+1][cCol] = new Cell(1, cRow+1, cCol);
+            if (maze[cRow+1][cCol] == null) maze[cRow+1][cCol] = new Cell(1, cRow+1, cCol);
             steps--;
             return pathGen(maze[cRow+1][cCol], steps);
         }
         else if (randDir.equals("left")) {
-            maze[cRow][cCol-1] = new Cell(1, cRow, cCol-1);
+            if (maze[cRow][cCol-1] == null) maze[cRow][cCol-1] = new Cell(1, cRow, cCol-1);
             steps--;
             return pathGen(maze[cRow][cCol-1], steps);
         }
         else {
-            maze[cRow][cCol+1] = new Cell(1, cRow, cCol+1);
+            if (maze[cRow][cCol+1] == null) maze[cRow][cCol+1] = new Cell(1, cRow, cCol+1);
             steps--;
             return pathGen(maze[cRow][cCol+1], steps);
         }
@@ -141,10 +138,14 @@ public class AMLJava extends JFrame
         textArea = new JTextArea(5, 1);
         textArea.setEditable(false);
         textArea.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        DefaultCaret caret = (DefaultCaret)textArea.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
         JPanel textPanel = new JPanel();
         textPanel.add(textArea);
         textPanel.setBackground(Color.WHITE);
         JScrollPane scrollPane = new JScrollPane(textPanel);
+        scrollPane.setPreferredSize(new Dimension(500, 100));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         add(scrollPane, BorderLayout.SOUTH);
         pack();
         setVisible(true);
@@ -156,6 +157,7 @@ public class AMLJava extends JFrame
         if (hasTop()) {
             move(maze[current.getRow()-1][current.getCol()]);
             textArea.append("Bot moved UP\n");
+            if (current.isTarget()) textArea.append("Bot moved on to a target!\n");
             return true;
         }
         else {
@@ -168,6 +170,7 @@ public class AMLJava extends JFrame
         if (hasBottom()) {
             move(maze[current.getRow()+1][current.getCol()]);
             textArea.append("Bot moved DOWN\n");
+            if (current.isTarget()) textArea.append("Bot moved on to a target!\n");
             return true;            
         }
         else {
@@ -179,7 +182,8 @@ public class AMLJava extends JFrame
     public static boolean move_L() {
         if (hasLeft()) {
             move(maze[current.getRow()][current.getCol()-1]);
-            textArea.append("Bot moved LEFT\n");
+            textArea.append("Bot moved LEFT\n");            
+            if (current.isTarget()) textArea.append("Bot moved on to a target!\n");
             return true;
         }        
         else {
@@ -192,6 +196,7 @@ public class AMLJava extends JFrame
         if (hasRight()) {
             move(maze[current.getRow()][current.getCol()+1]);
             textArea.append("Bot moved RIGHT\n");
+            if (current.isTarget()) textArea.append("Bot moved on to a target!\n");
             return true;
         }
         else {
@@ -209,10 +214,11 @@ public class AMLJava extends JFrame
         }
         catch (InterruptedException e) { }
         current.setText("");
-        if (current.isTarget()) current.setText("TARGET");
+        if (current.isTarget())  current.setText("TARGET");
+        if (current.isSource()) current.setText("START");
         current = next;
         current.visited();
-        current.setText("BOT");        
+        current.setText("BOT");
     }
     
     // returns true if there is a cell the bot can go on above it
@@ -245,42 +251,39 @@ public class AMLJava extends JFrame
         return false;
     }
     
-    // returns the cell to the right of the bot's current position IF it has been visited before (and exists)
+    // returns the cell to the right of the bot's current position if it exists
     public static Cell right() {
         if (hasRight()) {
             Cell c = maze[current.getRow()][current.getCol()+1];
-            if (c.getVisited()) return c;
-            else return null;
+            return c;
         }
         else return null;
     }
     
-    public static Cell top() {
+    public static Cell up() {
         if (hasTop()) {
             Cell c = maze[current.getRow()-1][current.getCol()];
-            if (c.getVisited()) return c;
-            else return null;
+            return c;
         }
         else return null;
     }
     
-    public static Cell bottom() {
-        if (hasRight()) {
+    public static Cell down() {
+        if (hasBottom()) {
             Cell c = maze[current.getRow()+1][current.getCol()];
-            if (c.getVisited()) return c;
-            else return null;
+            return c;
         }
         else return null;
     }
     
     public static Cell left() {
-        if (hasRight()) {
+        if (hasLeft()) {
             Cell c = maze[current.getRow()][current.getCol()-1];
-            if (c.getVisited()) return c;
-            else return null;
+            return c;
         }
         else return null;
     }
+    
     // returns whether or not the cell at row, col has been visited
     public static boolean visit(int row, int col) {
         return maze[row][col].getVisited();
